30a2910adc381388d173d6188410c233d243ad91
valezambra <zambranov556@gmail.com>
2023-04-22 02:59:20 -0300

    Agregado de dockerfile

--- src/test/java/com/example/BackEnd/BackEndApplicationTests.java
+++ src/test/java/com/example/BackEnd/BackEndApplicationTests.java
@@ -1,8 +1,13 @@
 package com.example.BackEnd;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
